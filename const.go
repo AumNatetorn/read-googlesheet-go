781c0c49d686d58007edea81432c92679aed181d
AumNatetorn <118706065+AumNatetorn@users.noreply.github.com>
2024-04-09 10:38:35 +0700

    Add files via upload

---
+++ const.go
@@ -0,0 +1,14 @@
+package model
+
+const (
+	LineURL               = "" //url Line
+	LineAccessToken       = "" // access token line noti
+	LineMessage           = "" // message line
+	StickerID             = 51626518
+	StickerPackageID      = 11538
+	StickerSuccess        = 16581277
+	StickerPackageSuccess = 8522
+	SheetID               = "" // google sheet ID
+	RangeSheet            = "" // range sheet
+	PageTitle             = "" //page google sheet
+)
